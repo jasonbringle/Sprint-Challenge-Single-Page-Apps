d54765ce34c631468f9d5e61c330f33756c44601
Jason <Jason@MacBook-Pro-3.local>
2020-02-22 11:49:19 -0800

    Locations added with same functionality as CharacterList... just refactored for proper dataset

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default function LocationsList() {}
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: blue;
+  font-size: 25px;
+`;
+const LocList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+export default function LocationList(props) {
+  console.log("LocList", props);
+  return (
+    <LocList>
+      {props.locate.map((loc, index) => {
+        return (
+          <StyledLink key={index} to={`/locate/${loc.id}`}>
+            {loc.name}
+          </StyledLink>
+        );
+      })}
+    </LocList>
+  );
+}
