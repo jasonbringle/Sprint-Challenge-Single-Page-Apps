4bfeba2a12b07933ebe24d53644da034f49a0ef7
Jason <Jason@MacBook-Pro-3.local>
2020-02-22 11:50:17 -0800

    Has same functionality as CharacterCard....just refactored for  location dataset.

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  height: auto;
+  box-shadow: 10px 10px 80px 10px black;
+  margin-top: 3rem;
+`;
+
+export default function LocationCard(props) {
+  const paramItemId = props.match.params.id;
+  // console.log('paramter ID', paramItemId)
+
+  const location = props.locate.find(item => {
+    return item.id === Number(paramItemId);
+  });
+  console.log("character", props.locate);
+
+  // console.log('CCprops', props.charData)
+
+  return location ? (
+    <Card>
+      <h1>Name:{location.name}</h1>
+      <p>Type: {location.type}</p>
+      <p>Dimension: {location.dimension}</p>
+    </Card>
+  ) : (
+    <p>404</p>
+  );
 }
