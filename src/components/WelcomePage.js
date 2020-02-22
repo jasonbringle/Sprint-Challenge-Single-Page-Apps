fec77bc645489f237345623da6ee547a7d5e187d
Jason <Jason@MacBook-Pro-3.local>
2020-02-22 09:21:48 -0800

    added styling to Welcome Page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Image = styled.img`
+  margin-top: 3rem;
+  width: 100%;
+  box-shadow: 10px 10px 80px 10px black;
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Image
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
