eb1085125a45acd999fe0d419458e20055f0cfd5
Jason <Jason@MacBook-Pro-3.local>
2020-02-22 11:46:12 -0800

    Seasrch Functionality uses a filter of included characters

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-export default function SearchForm() {
- 
+const App = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3rem;
+`;
+
+export default function SearchForm(props) {
+  const characters = props.chars.map(player => {
+    return player.name;
+  });
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(characters);
+
+  useEffect(() => {
+    const results = characters.filter(character => {
+      return character.toLowerCase().includes(searchTerm.toLowerCase());
+    },[characters]);
+
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <App>
+      <form>
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </form>
+      <div className="character-list">
+        <ul>
+          {searchResults.map(character => {
+            return <li key={character}>{character}</li>;
+          })}
+        </ul>
+      </div>
+    </App>
   );
 }
