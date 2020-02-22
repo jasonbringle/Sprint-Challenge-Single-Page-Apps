7862985d71b51546491c02d1c8fb84e4fc56f458
Jason <Jason@MacBook-Pro-3.local>
2020-02-22 09:13:25 -0800

    Added axios call and viewed data via console.log

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Header from "./components/Header.js";
-
+import axios from 'axios'
 
 export default function App() {
+
+  const [chars, setChar] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setChar(response.data.results);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }, []);
+console.log('chars', chars)
   return (
     <main>
       <Header />
